85e0a05491e5edb7d4263209b9fe5ecb50e46819
Sebastian Carrero <mugssebastian@gmail.com>
2023-08-14 22:13:59 -0500

    urls y paginas en django

--- cursoDjango/webpersonal/core/views.py
+++ cursoDjango/webpersonal/core/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+    <h1>Mi web personal</h1>
+    <ul>
+        <li><a href="/">Portada</a></li>
+        <li><a href="/about/">Acerca de</a></li>
+        <li><a href="/portafolio/">Mi portafolio</a></li>
+        <li><a href="/contacto/">Contacto</a></li>
+    </ul>
+"""
+
 # Create your views here.
 def home(request):
-    html_responseve = "<h1>Portada</h1>"
-    for i in range(8):
-        html_responseve += "<h2>lista</h2>"
+    html_responseve = (html_base +  "<h2>Portada</h2><p>Esta es la portada</p>")
     return HttpResponse(html_responseve)
+
+def about(request):
+    return HttpResponse(html_base +  "<h2>Acerca de</h2><p>Me llamo Sebastian y soy programador.</p>")
+
+def portafolio(request):
+    return HttpResponse(html_base +  "<h2>Mi portafolio</h2><p>Me llamo Sebastian y este es mi portafolio.</p>")
+
+def contacto(request):
+    return HttpResponse(html_base +  "<h2>Contacto</h2><p>Direccion #########.</p><p>Telefono ########.</p>")
--- cursoDjango/webpersonal/webpersonal/urls.py
+++ cursoDjango/webpersonal/webpersonal/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('portafolio/', views.portafolio, name="portafolio"),
+    path('contacto/', views.contacto, name="contacto"),
     path('admin/', admin.site.urls),
 ]
