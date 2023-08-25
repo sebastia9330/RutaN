9310c6bb1d3c67d64806b7469587eb144ca692b8
Sebastian Carrero <mugssebastian@gmail.com>
2023-08-24 23:58:43 -0500

    creacion de proyectos, edicion e imagenes

--- cursoDjango/webpersonal/webpersonal/settings.py
+++ cursoDjango/webpersonal/webpersonal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'portafolio',
+    'portafolio.apps.PortafolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
